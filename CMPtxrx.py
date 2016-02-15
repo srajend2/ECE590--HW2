b05f881763d098cb85465d3f48243ed754bb5241
Sunil Kumar <goforskr@gmail.com>
2016-02-15 01:45:00 -0500

    v1

--- CMPtxrx.py
+++ CMPtxrx.py
@@ -1,12 +1,15 @@
+
+# This program has to be run on the computer (Linux)
+
 import socket
 import time
 import re
 import os
+import binascii
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-					 
-MESSAGE = "<*5050.>" #Motor control message
+
 #'<*' is to notify header symbols and '.>' is to notify trailer symbols
 # the first two digits represent the speed of the left motor
 # the next two digits represent the speed of the right motor
@@ -14,11 +17,14 @@ MESSAGE = "<*5050.>" #Motor control message
 # 50 = motor stop
 # 51 to 99 = forward direction of motor, lowest speed at 51 and max speed at 99
 # 1 to 49 = reverse direction of motor, lowest speed at 1 and max speed at 49
+MESSAGE = "<*5050.>" #Motor control message
 
-ip = "192.168.1.247"
-ip1 = "192.168.1.37"
+# IP address of Raspberry PI
+ip = "192.168.1.170"
+
+# IP address of the computer (Linux)
+ip1 = "192.168.1.242"
 port = 5005
-port1 = 5006
 
 sock.sendto(MESSAGE, (ip, port))
 print "Sent 1 packet to %s through port %s successfully"%(ip, port)
@@ -28,32 +34,26 @@ sock = socket.socket(socket.AF_INET,
 sock.bind((ip1, port))
 
 packetnum = 0
-receivedimg = " "
+receivedimghex = " "
 while (packetnum < 586):
 	data, addr = sock.recvfrom(1024) 
-       # if data[0,3]=="HDR":
-       #    r = re.compile("([a-zA-Z]+)([0-9]+)")
-       #    m = r.match(data)
-       ##    m.group(1)
-       #    imglen= m.group(2)
         if data <> " ":
            packetnum = packetnum + 1
-           print "Received Packet %s", packetnum
-           receivedimg = receivedimg + data
+           print "Received Packet %s"%(packetnum)
+           receivedimghex = receivedimghex + data
        	   pass
 
-print "Received Image!!!, length is %s", len(receivedimg)
-reformatimg = receivedimg[0,599029]
+print "Received Image!!!, length is %s", len(receivedimghex)
+reformatimg = receivedimghex[1:599029]
 print "Reformatted Image!!!, length is ", len(reformatimg)
-#path1 = "ece590/pingpgms"
-#os.chdir( path1 )
-#file1 = 'ece590/pingpgms/rcvdrobot1.bmp'
-fo = open("ece590/pingpgms/rcvdrobot1.bmp",'wb')
+actualimg = binascii.unhexlify(reformatimg)
+
+fo = open("ece590/pingpgms/rcvdrobotfin.bmp",'wb')
 print "Name of the file: ", fo.name
-fo.write(receivedimg);
+fo.write(actualimg);
 fo.close()	
 
-file2 = "ece590/pingpgms/robot1.bmp"
+file2 = "ece590/pingpgms/rcvdrobotfin.bmp"
 import webbrowser
 webbrowser.open(file2)
 
--- RPItxrx.py
+++ RPItxrx.py
@@ -1,8 +1,14 @@
+# This program has to be run on Raspberry PI
+
 import socket
 import binascii
+import time
 
-UDP_IP = "192.168.1.247"
-UDP_IP2 = "192.168.1.37"
+# IP address of Raspberry PI
+UDP_IP = "192.168.1.170"
+
+# IP address of the computer (Linux)
+UDP_IP2 = "192.168.1.242"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -15,13 +21,6 @@ while True:
 	if data <> " ":
 	   break
 
-#for i in range(0, len(data)):
-#    print(data[i])
-print(len(data))
-print data[0]
-print data[1]
-print data[6]
-print data[7]
 packetnum = 0
 count1 = 0
 count2 = 1024
@@ -33,29 +32,14 @@ if (len(data)==8 and data[0]=="<" and data[1]=="*" and data[6]=="." and data[7]=
          content = f.read()
 #    print(binascii.hexlify(content))
     imgtext = binascii.hexlify(content)
-   # print "Image text length:",len(imgtext)
-   # imghdr="HDR"+str(len(imgtext))
-   # sock.sendto(imglenencode, (UDP_IP, UDP_PORT))
     while (packetnum < 586):
+          # To reduce packet drops at the listening end
+          time.sleep (20.0 / 1000.0);
           sock.sendto(imgtext[count1:count2], (UDP_IP2, UDP_PORT))
           count1 = count1 + 1024
           count2 = count2 + 1024
           packetnum = packetnum + 1
           
-    
-#    HOST = '127.0.0.1'
-#    PORT = 5005
-#    BUFSIZE = 1024
-#    ADDR = (HOST, PORT)
-#    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    serversock.bind(ADDR)
-#    serversock.listen(SOMAXCONN)
-#    fileOpen = open("/ece590/pingpgms/robot1.bmp")
-#    g = f.read()
-#    print 'Waiting For Connection..'
-#    clientsock, addr = serversock.accept()
-#    print 'Connection Established From: ', addr
-#   clientsock.sendall(fileOpen)
 else:
     print "Bad data!!! Please resend..."
 
